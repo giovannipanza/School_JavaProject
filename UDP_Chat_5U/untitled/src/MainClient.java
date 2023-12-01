dba03a195a4f02e80372766806f9cd2a0a141144
Giovanni Panza <giovanni.panza@tulliobuzzi.edu.it>
2023-12-01 15:14:17 +0100

    Delete UDP_Chat_5U/untitled/src directory

--- UDP_Chat_5U/untitled/src/MainClient.java
+++
@@ -1,8 +0,0 @@
-public class MainClient {
-    public static void main(String[] args) {
-        UDP_Client_Receive t1 = new UDP_Client_Receive();
-        UDP_Client_Send t2 = new UDP_Client_Send();
-        t1.start();
-        t2.start();
-    }
-}
--- UDP_Chat_5U/untitled/src/MainServer.java
+++
@@ -1,7 +0,0 @@
-public class MainServer {
-
-    public static void main(String[] args) {
-        UDP_Server server = new UDP_Server(9876);
-        server.startServer();
-    }
-}
