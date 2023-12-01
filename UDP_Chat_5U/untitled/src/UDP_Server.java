afbb4a988f854f8143c2b5cd1f686b8158f5d2f4
panzagiovanni <panzagiovanni@gmail.com>
2023-12-01 11:55:10 +0100

    versione 1

--- UDP_Chat_5U/untitled/src/UDP_Server.java
+++ UDP_Chat_5U/untitled/src/UDP_Server.java
@@ -3,13 +3,22 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UDP_Server {
     private DatagramSocket serverSocket;
-
+    private ArrayList<InetAddress> clients = new ArrayList<>();
+    private DatagramPacket receivePacket;
+    private DatagramPacket sendPacket;
+    private byte[] receiveData;
     public UDP_Server(int port) {
         try {
             serverSocket = new DatagramSocket(port);
+            receiveData = new byte[1024];
+            receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
             System.out.println("Server avviato...");
         } catch (Exception e) {
             e.printStackTrace();
@@ -19,35 +28,33 @@ public class UDP_Server {
     public void startServer() {
         try {
             while (true) {
-                byte[] receiveData = new byte[1024];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 serverSocket.receive(receivePacket);
-
                 String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
 
-                //prelevo l'indirizzo e la porta del client
                 InetAddress clientAddress = receivePacket.getAddress();
-                int clientPort = receivePacket.getPort();
+                clients.add(clientAddress);
 
-                System.out.println("Messaggio da " + clientAddress + ":" + clientPort + ": " + message);
+                System.out.println("Messaggio da " + clientAddress + ":" + clientAddress.getHostAddress()+ ": " + message);
 
-                broadcastMessage(message, clientPort);
+                broadcastMessage(message, clients);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void broadcastMessage(String message, int clientPort) {
+    private void broadcastMessage(String message,ArrayList<InetAddress> clients) {
         try {
             byte[] sendData = message.getBytes();
 
             // Invia il messaggio a tutti gli indirizzi IP sulla rete
-            InetAddress broadcastAddress = InetAddress.getByName("255.255.255.255");
+            for (InetAddress clientAddress : clients) {
+                int port = new InetSocketAddress(clientAddress, 0).getPort();
+                //ho il problema di come prelevare la porta del client
+                sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, port);
+                serverSocket.send(sendPacket);
+            }
 
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcastAddress, clientPort );
-
-            serverSocket.send(sendPacket);
         } catch (Exception e) {
             e.printStackTrace();
         }
